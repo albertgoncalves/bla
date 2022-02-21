35bb99769f1d27160be9a4ba85b318d41ed5a1f9
albertgoncalves <goncalves.albert@gmail.com>
2022-02-20 22:39:59 -0500

    move comment

--- src/com/Compile.hs
+++ src/com/Compile.hs
@@ -126,13 +126,13 @@ compileExpr context (AstExprUnOp _ op expr) =
 compileExpr context (AstExprBinOp _ l op r) =
   decrStackOffset $
     appendContextInsts (compileExpr (compileExpr context l) r) [binOpToInst op]
-compileExpr context0 (AstExprCall _ (AstExprVar _ "@print_char") args) =
-  appendContextInsts (foldl' compileExpr context0 args) [InstPrCh]
-compileExpr context0 (AstExprCall _ (AstExprVar _ "@print_i32") args) =
-  appendContextInsts (foldl' compileExpr context0 args) [InstPrI32]
 -- NOTE: Even though some functions do not return values, we always augment
 -- the stack here _as if_ that was true. We end up handling this when compiling
 -- `AstStmtEffect` statements below, which blindly decrements the stack offset.
+compileExpr context (AstExprCall _ (AstExprVar _ "@print_char") args) =
+  appendContextInsts (foldl' compileExpr context args) [InstPrCh]
+compileExpr context (AstExprCall _ (AstExprVar _ "@print_i32") args) =
+  appendContextInsts (foldl' compileExpr context args) [InstPrI32]
 compileExpr context0 (AstExprCall _ expr args) =
   appendContextInsts context2 [InstJump, PreInstLabelSet label]
     `setStackOffset` succ (getContextStackOffset context0)
